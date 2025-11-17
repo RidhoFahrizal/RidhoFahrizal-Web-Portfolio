5d6e94a7009ddc3717203da301cc212315297423
RidhoFahrizal <farizalridho@gmail.com>
2025-11-17 12:03:34 +0700

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RidhoFahrizal-Web-Portfolio/'
-
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['sea-turtle-app-pbb5x.ondigitalocean.app']
+  },
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: ['sea-turtle-app-pbb5x.ondigitalocean.app']
+  }
 })
